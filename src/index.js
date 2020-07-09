6b3c2812a99c1d8a5039e64d43e1d32138fd3991
Pedro A.Moreno <pedabraham@gmail.com>
2020-07-08 22:14:57 -0500

    Se levantó el estado de CheckButton a Resume, y se implementó filters

--- src/index.js
+++ src/index.js
@@ -3,26 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { CSSTransitionGroup } from 'react-transition-group';
 
 class CheckButton extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      skillOn: true,
+    //  skillOn: true,
     }
   }
 
-  handleClick(){
-    const skillState= this.state.skillOn;
-    this.setState({
-      skillOn: !skillState,
-    })
-  }
+  //handleClick(){
+    //const skillState= this.state.skillOn;
+    //this.setState({
+      //skillOn: !skillState,
+    //})
+  //}
 
   render(){
     const skill = this.props.skill;
     const button = (
-      <button className="skill-button" onClick ={()=>this.handleClick()}>
+      <button className="skill-button" onClick ={()=>this.props.onClick()}>
         <div className="skill-name">
             <div>{skill}</div>
             <div className="circle reject">
@@ -34,7 +35,7 @@ class CheckButton extends React.Component{
     );
 
     const buttonDimed =(
-      <button className={"skill-rejected skill-button"} onClick={()=>this.handleClick()}>
+      <button className={"skill-rejected skill-button"} onClick={()=>this.props.onClick()}>
         <div className="skill-name">
             <div>{skill}</div>
         </div>
@@ -42,16 +43,18 @@ class CheckButton extends React.Component{
       </button>
     )
 
-    return(this.state.skillOn ? button : buttonDimed)
+    return(this.props.skillOn ? button : buttonDimed)
   }
 }
 
 function TechStack(props) {
   const values = props.values;
-  const htmlStack = values.map((technology,i) => {
+  const htmlStack = values.map((skill,i) => {
+
+    const key="ts"+skill.name
     return(
-      <div>
-      <CheckButton skill={technology} />
+      <div key={key}>
+      <CheckButton  skill={skill.name} skillOn={skill.show} onClick={()=>props.onClick(i)}/>
       </div>
     )
   })
@@ -59,7 +62,7 @@ function TechStack(props) {
 }
 
 function Post(props){
-  let description = props.value.description.map(x=> <li>{x}</li>)
+  let description = props.value.description.map((x,i)=> <li key={props.title + i.toString()}>{x}</li>)
 
   return(
     <div className="post">
@@ -81,17 +84,28 @@ function Feed(props){
   const feed = list.map((post,i) => {
     const pline = <div className="PostLine"></div>
     if (i<list.length -1){
-      return([<Post value={post}/>,pline])
+      return(
+        <div key={post.title}>
+        <Post value={post}/>
+        {pline}
+        </div>
+      )
     }
     else{
-      return(<Post value={post}/>)
+      return(<div key={post.title}><Post value={post}/></div>)
     }
   })
   const logo = require('./Path.png');
   return(
     < >
-      {feed}
-      <img src={logo}  alt="Drawing" className="resumeFinalDraw"/>
+    <CSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+
+          {feed}
+          <img key={"garabato"} src={logo}  alt="Drawing" className="resumeFinalDraw"/>
+        </CSSTransitionGroup>
     </>
   )
 }
@@ -101,8 +115,6 @@ let entry1= {
         "place":"Continental AG,  September 2019 - february 2020",
         "description":["Built a system from the ground up to a proof of concept using sprints.","Developed the frontend of a car application using Python, jQuery, JavaScript, HTML, and CSS.","Designed multiple mockups using Adobe XD.","Use of git for version control."]
       };
-
-let entriesTest = [entry1,entry1,entry1,entry1,entry1,entry1]
 
 let entryTest2= [entry1,{
   "title":"Developer of Controller Designer",
@@ -122,24 +134,70 @@ let entryTest2= [entry1,{
 	"description":["Created an interface to open a strongbox using hand gestures by employing an array of CNY70 	sensors controlled by an FPGA described in, the hardware description language, VHDL."]
 }]
 
-function Resume(props){
 
-  return(
-    <div className="resume">
-      <div className="resume-header">
-        <div className="resume-title">Resume</div>
-        <div className="ResumeLine"></div>
+class Resume extends React.Component{
+  constructor(props){
+    const skills = ["Python","HTML","CSS","React","jQuery","VHDL","thin films"];
+    const skillJSON = skills.map(x=>({"name":x, "show": true}));
+
+    super(props);
+    this.state = {
+      skills : skillJSON,
+    }
+
+  }
+
+  handleClick(i){
+    const skills = this.state.skills;
+    const skillState= skills[i].show;
+    skills[i].show = !skillState;
+    //let feed = this.filterFeed(entryTest2,this.state.skills);
+    this.setState({
+      skills: skills,
+    })
+
+  }
+
+  filterFeed(rawFeed,skills){
+    let feed2pass = [];
+    let breakfor2 = false;
+    rawFeed.forEach((post) => {
+      for (var i = 0; i < post.description.length; i++) {
+        breakfor2 = false;
+        for (var j = 0; j < skills.length; j++) {
+          if (post.description[i].includes(skills[j].name) && skills[j].show) {
+            feed2pass = feed2pass.concat(post);
+            breakfor2=true;
+            break;
+          }
+        }
+        if(breakfor2){
+          break;
+        }
+      }
+    });
+    return feed2pass;
+  }
+
+  render(){
+    const feed = this.filterFeed(entryTest2,this.state.skills);
+    return(
+      <div className="resume">
+        <div className="resume-header">
+          <div className="resume-title">Resume </div>
+          <div className="ResumeLine"></div>
+        </div>
+        <div className="resume-lan">
+        </div>
+        <div className="resume-filters">
+          <TechStack values={this.state.skills}  onClick={(i)=>this.handleClick(i)} />
+        </div>
+        <div className="resume-feed">
+          <Feed values={feed}/>
+        </div>
       </div>
-      <div className="resume-lan">
-      </div>
-      <div className="resume-filters">
-        <TechStack values={["Python","HTML","CSS","React","C++"]} />
-      </div>
-      <div className="resume-feed">
-        <Feed values={entryTest2}/>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 
