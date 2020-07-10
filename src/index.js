c15465263412685c600510f5229756c3ea729d88
Pedro A.Moreno <pedabraham@gmail.com>
2020-07-10 13:16:20 -0500

    Se añadio la opcion de cambiar el lenguaje entre español e ingles

--- src/index.js
+++ src/index.js
@@ -143,6 +143,93 @@ let entryTest2= [{
 	"description":["Created an interface to open a strongbox using hand gestures by employing an array of CNY70 	sensors controlled by an FPGA described in, the hardware description language, VHDL."]
 }]
 
+const entradasEspañol = [
+  {
+    "title":"Desarrollador de este sitio",
+    "place":"Mi casa, pandemia 2020",
+    "description":["Diseñe bocetos del sitio usando notas de Apple", "Realicé maquetas del sitio usando Sketch", "Desarrollé el sitio mediante componentes usando React"]
+  },
+  {
+    "title":"Pasante de Innovación y Desarrollo",
+    "place": "Continental AG,  Septiembre 2019 - febrero 2020",
+    "description": ["Se creó un sistema desde 0 hasta la prueba de conceptos usando sprints.",
+    "Desarrollé el frontend del sistema usando HTML, CSS, JavaScript y jQuery.",
+    "Desarrollé en conjunto con mi equipo del backend usando Python y Flask.",
+    "Diseñé prototipos en adobe XD."]
+  },
+  {
+    "title":"Desarrollador de Controller Designer",
+    "place": "Universidad de Guanajuato, 2019",
+    "description":["Se creó una herramienta web para diseñar controladores clásicos usando HTML, 	CSS, JS y Python como el backend gracias a flask."],
+    "link":"https://github.com/pedabraham/ControlDiscreto"
+  },
+  {
+    "title":"Participante en una pasantía de verano de investigación con el trabajo “Fusión de imagnes multi-foco”",
+    "place":"Universidad de Guanajuato, 2018",
+    "description":["Se desarrolló un algoritmo para producir una imagen multi-foco por medio de la 	combimación de dos imágenes, de la misma escena y diferente enfoque, empleando 	diferentes técnicas de procesamiento de imágenes usando el lenguaje Python."],
+    "link":"http://www.jovenesenlaciencia.ugto.mx/index.php/jovenesenlaciencia/article/view/2826/2080"
+  },
+  {
+  "title":"Miembro del proyecto “Caracterización óptica de materiales”",
+  "place":"Universidad de Guanajuato, 2017",
+  "description":["Se propuso una metodología para sintetizar películas delgadas dopadas con líquidos iónicos obteniendo buenos resultados de absorción no lineal."]
+},
+{
+  "title":" Co-Desarrollador de MDSM",
+  "place": "Universidad de Guanajuato, 2016",
+  "description": ["Se creó una intefaz para abrir una cerradura usando gestos de la mano usando un 	arreglo de sensores CNY70 controlados mediante una FPGA descrita en VHDL."],
+  "link": "http://pedabraham.github.io/MDSM/"
+},
+]
+
+class Language extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      españolClass: "lan-button falseB",
+      englishClass: "lan-button"
+    }
+  }
+
+  lanSwitch(language){
+    if (language==="español") {
+      this.setState({
+        españolClass: "lan-button",
+        englishClass: "lan-button falseB"
+      })
+    }else if (language==="english") {
+      this.setState({
+        españolClass: "lan-button falseB",
+        englishClass: "lan-button "
+      })
+    }
+    this.props.onClick(language);
+  }
+
+
+
+  render(){
+    return(
+      <>
+        <button className={this.state.englishClass} onClick={()=>this.lanSwitch("english")}>
+          English
+        </button> /
+        <button className={this.state.españolClass} onClick={()=>this.lanSwitch("español")}>
+          Español
+        </button>
+      </>
+    )
+  }
+
+
+
+}
+
+
+
+
+
+
 
 class Resume extends React.Component{
   constructor(props){
@@ -152,8 +239,15 @@ class Resume extends React.Component{
     super(props);
     this.state = {
       skills : skillJSON,
+      language: "english"
     }
 
+  }
+
+  setLan(language){
+    this.setState({
+      language: language
+    })
   }
 
   handleClick(i){
@@ -189,14 +283,18 @@ class Resume extends React.Component{
   }
 
   render(){
-    const feed = this.filterFeed(entryTest2,this.state.skills);
+    const lanFeed = this.state.language==="english" ? entryTest2 : entradasEspañol;
+    const feed = this.filterFeed(lanFeed,this.state.skills);
     return(
       <div className="resume">
         <div className="resume-header">
-          <div className="resume-title">Resume </div>
+          <div className="resume-title">
+            {this.state.language==="english" ? "Resume" : "Currículum"}
+          </div>
           <div className="ResumeLine"></div>
         </div>
         <div className="resume-lan">
+          <Language onClick={(lan)=>this.setLan(lan)}/>
         </div>
         <div className="resume-filters">
           <TechStack values={this.state.skills}  onClick={(i)=>this.handleClick(i)} />
