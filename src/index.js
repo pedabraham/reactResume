f0738efdee478ed0e9ca40de18fdeccccd88fb02
Pedro A.Moreno <pedabraham@gmail.com>
2020-08-24 10:55:11 -0500

    Se movió el boton CheckButton al archivo removed.

--- src/index.js
+++ src/index.js
@@ -4,40 +4,6 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import { CSSTransitionGroup } from 'react-transition-group';
 
-class CheckButton extends React.Component{
-  constructor(props){
-    super(props);
-    this.state={
-    //  skillOn: true,
-    }
-  }
-
-  render(){
-    const skill = this.props.skill;
-    const button = (
-      <button className="skill-button" onClick ={()=>this.props.onClick()}>
-        <div className="skill-name">
-            <div>{skill}</div>
-            <div className="circle reject">
-                <div className="reject reject-line"></div>
-            </div>
-        </div>
-        <div className="skill-line"></div>
-      </button>
-    );
-
-    const buttonDimed =(
-      <button className={"skill-rejected skill-button"} onClick={()=>this.props.onClick()}>
-        <div className="skill-name">
-            <div>{skill}</div>
-        </div>
-        <div className="skill-line"></div>
-      </button>
-    )
-
-    return(this.props.skillOn ? button : buttonDimed)
-  }
-}
 
 class CheckButtonForm extends React.Component {
   render() {
