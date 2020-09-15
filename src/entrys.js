6cc9b5cdb5aecb28a04ffdcac08a4e523386ed07
Pedro A.Moreno <pedabraham@gmail.com>
2020-09-15 18:34:37 -0500

    Cambio de las entradas a un script separado

---
+++ src/entrys.js
@@ -0,0 +1,76 @@
+const entradasIngles= [
+  {
+    "title":"This site developer",
+    "place": "My home, 2020 pandemic",
+    "description":["Designed the sketches of the site using Apple notes.","Designed mockups of the site using Sketch.","Developed the site trough components using React.","Designed the style of the site using CSS."],
+
+  },{
+    "title":"Trend Antenna Intern",
+    "place":"Continental AG,  September 2019 - February 2020",
+    "description":["Built a system from the ground up to a proof of concept using sprints.","Developed the frontend of a car application using Python, jQuery, JavaScript, HTML, and CSS.","Designed multiple mockups using Adobe XD.","Use of git for version control."],
+    "pic":"https://www.dropbox.com/s/kkwtgdmd8jgb4qp/07BD2124-05B2-4A22-B734-B5A087E34E3C_1_105_c.jpeg?raw=1"
+  },{
+    "title":"Developer of Controller Designer",
+    "place": "University of Guanajuato, 2019",
+  	"description":["Created a web tool to design classic controllers using HTML, CSS, JavaScript, and Python as the backend thanks to flask.","Implemented the root-locus and frequency response methods to design controllers using Python."],
+    "link":   "https://github.com/pedabraham/ControlDiscreto",
+  },{
+    "title":"Author of “Multi-focus Image fusion using interpolations”",
+    "place":"Summer research program of the University of Guanajuato, 2018",
+  	"description":["Developed an algorithm to combine two images, of the same scene and a distinct level of focus, 	in a single image by employing several image processing techniques, using Python."],
+    "link":"http://www.jovenesenlaciencia.ugto.mx/index.php/jovenesenlaciencia/article/view/2826/2080"
+  },{
+    "title":"Member of project “Optical characterization of materials”",
+    "place":"University of Guanajuato, 2017",
+  	"description":["Modified, and tested, a methodology to synthesize thin films doped with an ionic liquid, 	obtaining nonlinear absorption as the more remarkable property."]
+  },{
+    "title":"Co-Developer of MDSM" ,
+    "place":"University of Guanajuato, 2016",
+  	"description":["Created a lock interface to use hand gestures by employing an array of CNY70 	sensors controlled by an FPGA described in, the hardware description language, VHDL."],
+    "link": "http://pedabraham.github.io/MDSM/"
+  }
+]
+
+const entradasEspañol = [
+  {
+    "title":"Desarrollador de este sitio",
+    "place":"Mi casa, pandemia 2020",
+    "description":["Diseñé bocetos del sitio usando notas de Apple", "Realicé maquetas del sitio usando Sketch", "Desarrollé el sitio mediante componentes usando React.","Diseñé el sitio usando CSS y JSX."]
+  },
+  {
+    "title":"Pasante del programa Trend Antenna",
+    "place": "Continental AG,  Septiembre 2019 - Febrero 2020",
+    "description": ["Se creó un sistema desde 0 hasta la prueba de conceptos usando sprints.",
+    "Desarrollé el frontend del sistema usando HTML, CSS, JavaScript y jQuery.",
+    "Desarrollé en conjunto con mi equipo del backend usando Python y Flask.",
+    "Diseñé múltiples maquetas usando Adobe XD."],
+    "pic":"https://www.dropbox.com/s/kkwtgdmd8jgb4qp/07BD2124-05B2-4A22-B734-B5A087E34E3C_1_105_c.jpeg?raw=1"
+  },
+  {
+    "title":  "Desarrollador de Controller Designer",
+    "place":  "Universidad de Guanajuato, 2019",
+    "description":["Creé una herramienta web para diseñar controladores clásicos (PI,PID, PD) usando HTML, CSS, JavaScript, y Python con flask.",
+                     "Implementé métodos de diseño de controladores como: lugar geométrico de las raíces y análisis de respuesta en frecuencia."],
+    "link":   "https://github.com/pedabraham/ControlDiscreto",
+
+  },
+  {
+    "title":"Autor del proyecto “Fusión de imágenes multi-foco usando interpolaciones polinominales”",
+    "place":"Verano de investigación de la Universidad de Guanajuato, 2018",
+    "description":[" Desarrollé un algoritmo capaz de combinar dos imágenes, de la misma escena y distinto nivel 	de enfoque, en una sola empleando diversas técnicas de procesamiento de imágenes usando Python con librerías como NumPy y OpenCV."],
+    "link":"http://www.jovenesenlaciencia.ugto.mx/index.php/jovenesenlaciencia/article/view/2826/2080"
+  },
+  {
+  "title":"Miembro del proyecto “Caracterización óptica de materiales”",
+  "place":"Universidad de Guanajuato, 2017",
+  "description":["Se propuso una metodología para sintetizar películas delgadas (thin films) dopadas con líquidos iónicos, obteniendo como propiedad mas destacable la absorción no lineal."]
+},
+{
+  "title":" Co-Desarrollador de MDSM",
+  "place": "Universidad de Guanajuato, 2016",
+  "description": [" Creé una interfaz de cerradura, para usar gestos de la mano, empleando un arreglo de 	componentes CNY70 conectados a un FPGA cuyo código estaba descrito en VHDL."],
+  "link": "http://pedabraham.github.io/MDSM/"
+},
+]
+
+export {entradasIngles,entradasEspañol}
